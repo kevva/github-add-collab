98f40cb70d5a1084ab39cf0764dce48d50de2709
Kevin Martensson <kevinmartensson@gmail.com>
2015-04-24 15:48:41 +0200

    Add `gh-got` instead of `got`

--- index.js
+++ index.js
@@ -2,29 +2,21 @@
 
 var githubRepos = require('github-repositories');
 var githubTokenUser = require('github-token-user');
-var got = require('got');
+var ghGot = require('gh-got');
 var eachAsync = require('each-async');
-var objectAssign = require('object-assign');
-
-function addCollab(user, repo, opts, cb) {
-	var url = [
-		'https://api.github.com/repos/' + repo + '/',
-		'collaborators/' + user
-	].join('');
-
-	got.put(url, {headers: opts.headers}, function (err) {
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		cb();
-	});
-}
 
 function run(user, repos, opts, cb) {
 	eachAsync(repos, function (repo, i, next) {
-		addCollab(user, repo, opts, next);
+		var url = 'repos/' + repo + '/collaborators/' + user;
+
+		ghGot.put(url, {token: opts.token}, function (err, data) {
+			if (err) {
+				cb(err);
+				return;
+			}
+
+			cb();
+		});
 	}, function (err) {
 		if (err) {
 			cb(err);
@@ -57,6 +49,8 @@ function getRepos(user, login, opts, cb) {
 }
 
 module.exports = function (user, repos, opts, cb) {
+	opts = opts || {};
+
 	if (typeof user !== 'string') {
 		throw new Error('User required');
 	}
@@ -79,24 +73,14 @@ module.exports = function (user, repos, opts, cb) {
 		throw new Error('`addToAll` and `addToSources` cannot be used with `repos`');
 	}
 
-	opts = objectAssign({}, opts);
-	opts.headers = {
-		Accept: 'application/vnd.github.v3+json',
-		Authorization: 'token ' + opts.token,
-		'Content-Length': 0,
-		'User-Agent': 'https://github.com/kevva/github-add-collab'
-	};
-
 	githubTokenUser(opts.token, function (err, data) {
-		var username = data.login;
-
 		if (!repos.length && (opts.addToAll || opts.addToSources)) {
-			getRepos(user, username, opts, cb);
+			getRepos(user, data.login, opts, cb);
 			return;
 		}
 
 		repos = repos.map(function (repo) {
-			return repo.split('/')[1] ? repo : username + '/' + repo;
+			return repo.split('/')[1] ? repo : data.login + '/' + repo;
 		});
 
 		run(user, repos, opts, cb);
