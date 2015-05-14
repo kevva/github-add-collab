8e61eeeda6d4a55dcabe7e47dc62b556d25477a7
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-14 16:00:21 +0200

    Add tests

--- test.js
+++ test.js
@@ -1,9 +1,40 @@
 'use strict';
 
-var githubAddCollab = require('./');
 var test = require('ava');
+var githubAddCollab = require('./');
 
-test(function (t) {
-	t.assert(true);
-	t.end();
+test('add collab to repository', function (t) {
+	t.plan(3);
+
+	githubAddCollab('kevva', ['unicorns'], {
+		token: process.env.GITHUB_TOKEN
+	}, function (err, data) {
+		t.assert(!err, err);
+		t.assert(data.length === 1);
+		t.assert(data[0] === 'unicornbot/unicorns');
+	});
+});
+
+test('add collab to all repositories', function (t) {
+	t.plan(2);
+
+	githubAddCollab('kevva', {
+		token: process.env.GITHUB_TOKEN,
+		addToAll: true
+	}, function (err, data) {
+		t.assert(!err, err);
+		t.assert(data.length === 2);
+	});
+});
+
+test('add collab to all source repositories', function (t) {
+	t.plan(2);
+
+	githubAddCollab('kevva', {
+		token: process.env.GITHUB_TOKEN,
+		addToSources: true
+	}, function (err, data) {
+		t.assert(!err, err);
+		t.assert(data.length === 1);
+	});
 });
