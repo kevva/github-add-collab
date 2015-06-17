45849aee7c879ff6e4e72ed19918d8b2d3e16cf8
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-17 16:45:08 +0200

    Fix tests
    Fixes #7.

--- test.js
+++ test.js
@@ -3,26 +3,26 @@ var test = require('ava');
 var githubAddCollab = require('./');
 
 test('add collab to repository', function (t) {
-	t.plan(3);
+	t.plan(2);
 
 	githubAddCollab('kevva', ['unicorns'], {
 		token: process.env.GITHUB_TOKEN
 	}, function (err, data) {
 		t.assert(!err, err);
-		t.assert(data.length === 1);
-		t.assert(data[0] === 'unicornbot/unicorns');
+		t.assert(data.indexOf('unicorn/unicorns'));
 	});
 });
 
 test('add collab to all repositories', function (t) {
-	t.plan(2);
+	t.plan(3);
 
 	githubAddCollab('kevva', {
 		token: process.env.GITHUB_TOKEN,
 		addToAll: true
 	}, function (err, data) {
 		t.assert(!err, err);
-		t.assert(data.length === 2);
+		t.assert(data.indexOf('unicorn/unicorns'));
+		t.assert(data.indexOf('unicorn/unicorn'));
 	});
 });
 
@@ -34,6 +34,6 @@ test('add collab to all source repositories', function (t) {
 		addToSources: true
 	}, function (err, data) {
 		t.assert(!err, err);
-		t.assert(data.length === 1);
+		t.assert(data.indexOf('unicorn/unicorns'));
 	});
 });
