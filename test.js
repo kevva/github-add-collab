05cfcab4d8e7959112e9cd9f1cfb6634ef04332c
Kevin Martensson <kevinmartensson@gmail.com>
2015-10-21 14:19:37 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,36 +1,29 @@
-'use strict';
-var test = require('ava');
-var githubAddCollab = require('./');
+import test from 'ava';
+import githubAddCollab from './';
 
-test('add collab to repository', function (t) {
-	t.plan(1);
-
-	githubAddCollab('kevva', ['unicorns'], {
+test('add collab to repository', async t => {
+	const data = await githubAddCollab('kevva', ['unicorns'], {
 		token: process.env.GITHUB_TOKEN
-	}).then(function (data) {
-		t.assert(data.indexOf('unicorn/unicorns'));
 	});
+
+	t.ok(data.includes('unicorn/unicorns'));
 });
 
-test('add collab to all repositories', function (t) {
-	t.plan(2);
-
-	githubAddCollab('kevva', {
+test('add collab to all repositories', async t => {
+	const data = await githubAddCollab('kevva', {
 		token: process.env.GITHUB_TOKEN,
 		addToAll: true
-	}).then(function (data) {
-		t.assert(data.indexOf('unicorn/unicorns'));
-		t.assert(data.indexOf('unicorn/unicorn'));
 	});
+
+	t.ok(data.includes('unicorn/unicorns'));
+	t.ok(data.includes('unicorn/unicorn'));
 });
 
-test('add collab to all source repositories', function (t) {
-	t.plan(1);
-
-	githubAddCollab('kevva', {
+test('add collab to all source repositories', async t => {
+	const data = await githubAddCollab('kevva', {
 		token: process.env.GITHUB_TOKEN,
 		addToSources: true
-	}).then(function (data) {
-		t.assert(data.indexOf('unicorn/unicorns'));
 	});
+
+	t.ok(data.includes('unicorn/unicorns'));
 });
